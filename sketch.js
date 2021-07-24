e81fd31c50144d43c80030223e91380cafe962a7
Nishkal <72551241+Nishkal467@users.noreply.github.com>
2021-07-24 23:06:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,8 +10,8 @@ var question, contestant, quiz;
 
 
 function setup(){
-  canvas = createCanvas(850,400);
   database = firebase.database();
+  canvas = createCanvas(850,400);
   quiz = new Quiz();
   quiz.getState();
   quiz.start();
